9e6d03311875808a57b5bd7d610fd40aba96baf2
vhoyer <vhoyer@live.com>
2016-11-14 16:20:14 -0200

    Added message if no results
    Signed-off-by: vhoyer <vhoyer@live.com>

--- pesq.aspx
+++ pesq.aspx
@@ -5,18 +5,21 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head runat="server">
 		<title>Resultado da Pesquisa</title>
+		<link type='text/css' rel='stylesheet' href='style/main.css' />
 	</head>
 	<body>
 		<form id="form1" runat="server">
-			<div id='results_container' runat='server'>
-				<!--<div class='result_entry'>-->
-					<!--<a href=''><img src='images/userProfile/profile-picture-placeholder.png' /></a>-->
+			<article>
+				<div id='results_container' runat='server'>
+					<!--<div class='result_entry'>-->
+					<!--<a href='' class='profile_img_wrapper'><img src='images/userProfile/profile-picture-placeholder.png' /></a>-->
 					<!--<div class='res_text'>-->
-						<!--<a href=''><h5 class='res_title'>Aqui vai o nome</h5></a>-->
-						<!--<span class='res_bio'>Aqui vai aquele textinho</span>-->
+					<!--<a href=''><h5 class='res_title'>Aqui vai o nome</h5></a>-->
+					<!--<span class='res_bio'>Aqui vai aquele textinho</span>-->
 					<!--</div>-->
-				<!--</div>-->
-			</div>
+					<!--</div>-->
+				</div>
+			</article>
 			<asp:SqlDataSource ID="Sqlds1" runat="server" ConnectionString="<%$ ConnectionStrings:rinfConnectionString %>" ProviderName="<%$ ConnectionStrings:rinfConnectionString.ProviderName %>">
 			</asp:SqlDataSource>
 		</form>
--- pesq.aspx.cs
+++ pesq.aspx.cs
@@ -40,37 +40,43 @@ public partial class pesq : System.Web.UI.Page
 
 		//finds the control where we'll be putting the result stuff
 		Control container = FindControl("results_container");
-		foreach(DataRow result in dt.Rows)
+		if (dt.Rows.Count != 0)
+			foreach(DataRow result in dt.Rows)
+			{
+				//create the objects that will be put in the page
+				WebControl entry = new WebControl(HtmlTextWriterTag.Div),
+						   res_text = new WebControl(HtmlTextWriterTag.Div),
+						   pic = new WebControl(HtmlTextWriterTag.Img),
+						   anchor1 = new WebControl(HtmlTextWriterTag.A),
+						   anchor2 = new WebControl(HtmlTextWriterTag.A),
+						   name = new WebControl(HtmlTextWriterTag.H5),
+						   res_bio = new WebControl(HtmlTextWriterTag.Span);
+
+				//set the attributes to the objects
+				entry.CssClass = "result_entry";
+				pic.Attributes["src"] = FileName.ImgFolder + result["img"].ToString();
+				string ahref = FileName.Profile+"?user="+result["email"].ToString();
+				anchor1.Attributes["href"] = ahref;
+				anchor1.Attributes["class"] = "profile_img_wrapper";
+				anchor2.Attributes["href"] = ahref;
+				name.Controls.Add(new LiteralControl(result["name"].ToString()));
+				res_bio.Controls.Add(new LiteralControl(result["bio"].ToString()));
+
+				//adds each control to it's parent and so
+				anchor1.Controls.Add(pic);
+
+				anchor2.Controls.Add(name);
+				res_text.Controls.Add(anchor2);
+				res_text.Controls.Add(res_bio);
+
+				entry.Controls.Add(anchor1);
+				entry.Controls.Add(res_text);
+
+				container.Controls.Add(entry);
+			}
+		else
 		{
-			//create the objects that will be put in the page
-			WebControl entry = new WebControl(HtmlTextWriterTag.Div),
-					   res_text = new WebControl(HtmlTextWriterTag.Div),
-					   pic = new WebControl(HtmlTextWriterTag.Img),
-					   anchor1 = new WebControl(HtmlTextWriterTag.A),
-					   anchor2 = new WebControl(HtmlTextWriterTag.A),
-					   name = new WebControl(HtmlTextWriterTag.H5),
-					   res_bio = new WebControl(HtmlTextWriterTag.Span);
-
-			//set the attributes to the objects
-			entry.CssClass = "result_entry";
-			pic.Attributes["src"] = FileName.ImgFolder + result["img"].ToString();
-			string ahref = FileName.Profile+"?user="+result["email"].ToString();
-			anchor1.Attributes["href"] = ahref;
-			anchor2.Attributes["href"] = ahref;
-			name.Controls.Add(new LiteralControl(result["name"].ToString()));
-			res_bio.Controls.Add(new LiteralControl(result["bio"].ToString()));
-
-			//adds each control to it's parent and so
-			anchor1.Controls.Add(pic);
-
-			anchor2.Controls.Add(name);
-			res_text.Controls.Add(anchor2);
-			res_text.Controls.Add(res_bio);
-
-			entry.Controls.Add(anchor1);
-			entry.Controls.Add(res_text);
-
-			container.Controls.Add(entry);
+			container.Controls.Add(new LiteralControl("Nenhum resultado encontrado."));
 		}
 	}
 }
